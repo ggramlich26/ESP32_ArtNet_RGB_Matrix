2359d2be1a344edfdcdb644482d0a5bef4e34734
ggramlich26 <14877069+ggramlich26@users.noreply.github.com>
2021-09-28 23:41:10 +0200

    Included current config in main web page

--- Webserver.cpp
+++ Webserver.cpp
@@ -19,7 +19,7 @@
 
 String handle_internet_settings_request(AsyncWebServerRequest *request);
 String handle_led_settings_request(AsyncWebServerRequest *request);
-String create_config_html();
+String create_config_table_only();
 
 AsyncWebServer server(80);
 
@@ -34,86 +34,57 @@ const char* NUMBER_LEDS_INPUT = "input_number_leds";
 const char* SHORT_DESCRIPTOR_INPUT = "input_short_descriptor";
 const char* LONG_DESCRIPTOR_INPUT = "input_long_descriptor";
 
-// HTML web page to handle 3 input fields (input1, input2, input3)
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML>
-<html>
-<head>
-	<title>LED controller settings</title>
-	<style>
-		form  { display: table;      }
-		p     { display: table-row;  }
-		label { display: table-cell; }
-		input { display: table-cell; }
-	</style>
-</head>
 
-<body>
-	<a href="/config">Show configuration</a>
-	<form action="/get" autocomplete="off">
-		<p style="font-size:35px">
-			<label for="input_led_output">LED output to update: &nbsp </label>
-			<input style="font-size:35px" name="input_led_output" min=0 type="number">
-		</p>
-		<p style="font-size:35px">
-			<label for="input_artnet_universe">ArtNet universe: &nbsp </label>
-			<input style="font-size:35px" name="input_artnet_universe" min=0 type="number">
-		</p>
-		<p style="font-size:35px">
-			<label for="input_dmx_addr">DMX address: &nbsp </label>
-			<input style="font-size:35px" name="input_dmx_addr" min="1" max="512" type="number">
-		</p>
-		<p style="font-size:35px">
-			<label for="input_number_leds">Number LEDs: &nbsp </label>
-			<input style="font-size:35px" name="input_number_leds" min="0" max="300" type="number">
-		</p>
-		<p style="font-size:35px">
-			<label for="input_short_descriptor">Short descriptor: &nbsp </label>
-			<input style="font-size:35px" name="input_short_descriptor" maxlength=18 type="text">
-		</p>
-		<p style="font-size:35px">
-			<label for="input_long_descriptor">Long descriptor: &nbsp </label>
-			<input style="font-size:35px" name="input_long_descriptor" maxlength=64 type="text">
-		</p>
-		<br>
-		<p>
-			<input style="font-size:35px" type="submit" value="Set LED settings">
-		</p>
-	</form>
-	<br>
-	<br>
-	<form action="/get" autocomplete="off">
-		<p style="font-size:35px">
-			<label for="input_ssid">New SSID: </label>
-			<input style="font-size:35px" name="input_ssid" maxlength=30 type="text">
-		</p>
-		<p style="font-size:35px">
-			<label for="input_password">New password: &nbsp </label>
-			<input style="font-size:35px" name="input_password" maxlength=30 type="text">
-		</p>
-		<p style="font-size:35px">
-			<label for="input_host_name">New host name: &nbsp </label>
-			<input style="font-size:35px" name="input_host_name" maxlength=30 type="text">
-		</p>
-		<p style="font-size:35px">
-			Internet mode:
-		</p>
-		<p style="font-size:35px">
-			<input style="font-size:35px" name="input_internet_mode_wifi_dhcp" type="radio">
-			<label for="input_internet_mode_wifi_dhcp">WiFi DHCP &nbsp </label>
-			<input style="font-size:35px" name="input_internet_mode_ethernet_dhcp" type="radio">
-			<label for="input_internet_mode_ethernet_dhcp">Ethernet DHCP &nbsp </label>
-			<input style="font-size:35px" name="input_internet_mode_access_point" type="radio">
-			<label for="input_internet_mode_access_point">Access point &nbsp </label>
-		</p>
-		<p>
-			<input style="font-size:35px" type="submit" value="Set Internet settings">
-		</p>
-	</form>
-</body>
+String StringIndexHtmlStart ="<!DOCTYPE HTML> <html> <head> <title>LED controller settings</title> "
+		"<style> form { display: table; } p { display: table-row; } label { display: table-cell; } "
+		"input { display: table-cell; } "
 
-</html>
-)rawliteral";
+			"table { font-family: arial, sans-serif; border-collapse: collapse; width: 100%; } "
+			"td, th { border: 1px solid #dddddd; text-align: left; padding: 8px; } tr:nth-child(even) "
+			"{ background-color: #dddddd; } "
+
+		"</style> </head> "
+		"<body>  ";
+
+String StringIndexHtmlEnd =
+		"<br>"
+		"<h2>Edit configuration</h2> "
+		"<form action=\"/get\" autocomplete=\"off\"> "
+		"<p style=\"font-size:35px\"> "
+		"<label for=\"input_led_output\">LED output to update: &nbsp </label> "
+		"<input style=\"font-size:35px\" name=\"input_led_output\" min=0 type=\"number\"> </p> "
+		"<p style=\"font-size:35px\"> "
+		"<label for=\"input_artnet_universe\">ArtNet universe: &nbsp </label> "
+		"<input style=\"font-size:35px\" name=\"input_artnet_universe\" min=0 type=\"number\"> </p> "
+		"<p style=\"font-size:35px\"> <label for=\"input_dmx_addr\">DMX address: &nbsp </label> "
+		"<input style=\"font-size:35px\" name=\"input_dmx_addr\" min=\"1\" max=\"512\" type=\"number\"> </p> "
+		"<p style=\"font-size:35px\"> <label for=\"input_number_leds\">Number LEDs: &nbsp </label> "
+		"<input style=\"font-size:35px\" name=\"input_number_leds\" min=\"0\" max=\"300\" type=\"number\"> </p> "
+		"<p style=\"font-size:35px\"> <label for=\"input_short_descriptor\">Short descriptor: &nbsp </label> "
+		"<input style=\"font-size:35px\" name=\"input_short_descriptor\" maxlength=18 type=\"text\"> </p> "
+		"<p style=\"font-size:35px\"> <label for=\"input_long_descriptor\">Long descriptor: &nbsp </label> "
+		"<input style=\"font-size:35px\" name=\"input_long_descriptor\" maxlength=64 type=\"text\"> </p> <br> "
+		"<p> <input style=\"font-size:35px\" type=\"submit\" value=\"Set LED settings\"> </p> "
+		"</form> "
+		"<br> <br> "
+		"<form action=\"/get\" autocomplete=\"off\"> <p style=\"font-size:35px\"> "
+		"<label for=\"input_ssid\">New SSID: </label> "
+		"<input style=\"font-size:35px\" name=\"input_ssid\" maxlength=30 type=\"text\"> </p> "
+		"<p style=\"font-size:35px\"> <label for=\"input_password\">New password: &nbsp </label> "
+		"<input style=\"font-size:35px\" name=\"input_password\" maxlength=30 type=\"text\"> </p> "
+		"<p style=\"font-size:35px\"> <label for=\"input_host_name\">New host name: &nbsp </label> "
+		"<input style=\"font-size:35px\" name=\"input_host_name\" maxlength=30 type=\"text\"> </p> "
+		"<p style=\"font-size:35px\"> Internet mode: </p> <p style=\"font-size:35px\"> "
+		"<input style=\"font-size:35px\" name=\"input_internet_mode_wifi_dhcp\" type=\"radio\"> "
+		"<label for=\"input_internet_mode_wifi_dhcp\">WiFi DHCP &nbsp </label> "
+		"<input style=\"font-size:35px\" name=\"input_internet_mode_ethernet_dhcp\" type=\"radio\"> "
+		"<label for=\"input_internet_mode_ethernet_dhcp\">Ethernet DHCP &nbsp </label> "
+		"<input style=\"font-size:35px\" name=\"input_internet_mode_access_point\" type=\"radio\"> "
+		"<label for=\"input_internet_mode_access_point\">Access point &nbsp </label> </p> "
+		"<p> <input style=\"font-size:35px\" type=\"submit\" value=\"Set Internet settings\"> </p> "
+		"</form> "
+		"</body> "
+		"</html>";
 
 void notFound(AsyncWebServerRequest *request) {
 	request->send(404, "text/plain", "Not found");
@@ -123,10 +94,7 @@ void notFound(AsyncWebServerRequest *request) {
 void webserver_init(){
 	// Send web page with input fields to client
 	server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-		request->send_P(200, "text/html", index_html);
-	});
-	server.on("/config", HTTP_GET, [](AsyncWebServerRequest *request){
-		request->send(200, "text/html", create_config_html());
+		request->send(200, "text/html", StringIndexHtmlStart + create_config_table_only() + StringIndexHtmlEnd);
 	});
 
 	server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
@@ -134,24 +102,6 @@ void webserver_init(){
 		if(reply.equals("")){
 			reply = handle_led_settings_request(request);
 		}
-//		String new_ssid;
-//		String new_password;
-//		String new_host_name;
-//		// GET newnew  SSID value on <ESP_IP>/get?input_ssid=<inputMessage>
-//		if (request->hasParam(SSID_INPUT)) {
-//			new_ssid = request->getParam(SSID_INPUT)->value();
-//		}
-//		// GET new password value on <ESP_IP>/get?input_password=<inputMessage>
-//		if (request->hasParam(PASSWORD_INPUT)) {
-//			new_password = request->getParam(PASSWORD_INPUT)->value();
-//		}
-//		// GET new host name value on <ESP_IP>/get?input_host_name=<inputMessage>
-//		if (request->hasParam(HOST_NAME_INPUT)) {
-//			new_host_name = request->getParam(HOST_NAME_INPUT)->value();
-//		}
-//		String reply = DataManager::setWIFICredentials((new_ssid != NULL?new_ssid.c_str():""),
-//				(new_password != NULL?new_password.c_str():""),
-//				(new_host_name != NULL?new_host_name.c_str():""));
 		request->send(200, "text/html", "<p style=\"font-size:25\">" + reply + "</p>"
 				"<br><a href=\"/\">Return</a>");
 	});
@@ -181,12 +131,6 @@ String handle_internet_settings_request(AsyncWebServerRequest *request){
 		new_host_name = request->getParam(HOST_NAME_INPUT)->value();
 		update = true;
 	}
-//			<input style="font-size:35px" name="input_internet_mode_wifi_dhcp" type="radio">
-//			<label for="input_internet_mode_wifi_dhcp">WiFi DHCP &nbsp </label>
-//			<input style="font-size:35px" name="input_internet_mode_ethernet_dhcp" type="radio">
-//			<label for="input_internet_mode_ethernet_dhcp">Ethernet DHCP &nbsp </label>
-//			<input style="font-size:35px" name="input_internet_mode_access_point" type="radio">
-//			<label for="input_internet_mode_access_point">Access point &nbsp </label>
 	if(request->hasParam("input_internet_mode_wifi_dhcp")){
 		newMode = wifiDHCP;
 		updateMode = true;
@@ -252,20 +196,12 @@ String handle_led_settings_request(AsyncWebServerRequest *request){
 	return reply;
 }
 
-String create_config_html(){
-	String html = String("<!DOCTYPE html> <html> <head> "
-			"<style> "
-			"table { font-family: arial, sans-serif; border-collapse: collapse; width: 100%; } "
-			"td, th { border: 1px solid #dddddd; text-align: left; padding: 8px; } tr:nth-child(even) "
-			"{ background-color: #dddddd; } "
-			"</style> "
-			"</head> "
-			"<body> "
-			"<h2>LED controller configuration</h2> "
+String create_config_table_only(){
+	String html =
+			"<h2>Current configuration</h2> "
 			"<table> "
 			"<tr> <th>Output</th> <th>ArtNet universe</th> <th>DMX address</th> "
-			"<th>Number LEDs</th> <th>Short descriptor</th> <th>Long descriptor</th> </tr> ");
-
+			"<th>Number LEDs</th> <th>Short descriptor</th> <th>Long descriptor</th> </tr> ";
 	for(int i = 0; i < NUMBER_LED_OUTPUTS; i++){
 		html += "<tr> "
 				"<td> " + String(i) + "</td> " +
@@ -276,10 +212,7 @@ String create_config_html(){
 				"<td> " + DataManager::getLedConfig(i)->longName + "</td> "
 				"</tr> ";
 	}
-	html += "</table> "
-	"<br><a href=\"/\">Return</a>"
-	"</body> "
-	"</html>";
+	html += "</table> ";
 	return html;
 }
 
