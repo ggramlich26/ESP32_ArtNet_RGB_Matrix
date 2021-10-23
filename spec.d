e34f806d284697ec7d44cd1d672129f7e3e827d7
ggramlich26 <14877069+ggramlich26@users.noreply.github.com>
2021-10-23 14:22:54 +0200

    project config files

--- spec.d
+++ spec.d
@@ -1,2 +1,2 @@
 spec.o: \
- /media/persDat/Mikrocontroller/Sloeber/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.C
+ C:/Users/ag4716/Documents/sloeber-workspace/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.C
